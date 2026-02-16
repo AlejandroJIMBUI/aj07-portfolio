25fd88b2504c53ca2e5ee7377d3f18df6e8eecb6
AlejandroJ07 <buitragojherson22@gmail.com>
2026-02-16 17:13:12 -0500

    Initial commit: Astro setup

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  site: "https://AlejandroJIMBUI.github.io",
+  base: "/aj07-portfolio",
+  output: "static",
+});
