13810d7b18e631781be39c7a17b4068017bc96a2
AlejandroJ07 <buitragojherson22@gmail.com>
2026-02-25 19:12:37 -0500

    Update: create In Development Page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://AlejandroJIMBUI.github.io",
-  base: "/aj07-portfolio",
+  // coment lines 6 & 7 for development
+  //site: "https://AlejandroJIMBUI.github.io",
+  //base: "/aj07-portfolio",
   output: "static",
 });
