946395599877b30e4327aa420f86527fe6b27a5f
AlejandroJ07 <buitragojherson22@gmail.com>
2026-02-25 19:26:24 -0500

    Update: clear coments in astro.config.ms

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   // coment lines 6 & 7 for development
-  //site: "https://AlejandroJIMBUI.github.io",
-  //base: "/aj07-portfolio",
+  site: "https://AlejandroJIMBUI.github.io",
+  base: "/aj07-portfolio",
   output: "static",
 });
